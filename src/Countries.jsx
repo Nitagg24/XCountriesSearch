fb8f2d6f012f306ded60abf9a8d6e5da328c130b
nitin aggarwal <agg.nitin124@gmail.com>
2024-08-20 01:47:56 +0530

    final commit

---
+++ src/Countries.jsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react";
+
+const CountryCard = ({ name, flag, altText }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "4px",
+        border: "1px solid black",
+        borderRadius: "8px",
+        height: "200px",
+        width: "200px",
+        margin: "10px",
+        padding: "10px",
+        textAlign: "center",
+      }}
+    >
+      <img
+        src={flag}
+        alt={altText}
+        style={{
+          height: "100px",
+          width: "100px",
+        }}
+      />
+      <h2>{name}</h2>
+    </div>
+  );
+};
+
+const URL_API = "https://restcountries.com/v3.1/all";
+
+function Countires() {
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(URL_API);
+        const jsonRes = await response.json();
+        console.log(jsonRes);
+        setCountries(jsonRes);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleSearch = (e) => {
+    const value = e.target.value;
+    clearTimeout(window.debounceTimeout);
+    window.debounceTimeout = setTimeout(() => {
+      setSearch(value);
+    }, 500);
+  };
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(search.toLowerCase())
+  );
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search for a country..."
+        onChange={handleSearch}
+        style={{
+          width: "100%",
+          padding: "10px",
+          marginBottom: "20px",
+          fontSize: "16px",
+          borderRadius: "4px",
+          border: "1px solid #ccc",
+        }}
+      />
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        {filteredCountries.length > 0 ? (
+          filteredCountries.map((country) => (
+            <CountryCard
+              key={country.cca3}
+              name={country.name.common}
+              flag={country.flags.png}
+              altText={country.name.common}
+            />
+          ))
+        ) : (
+          <p>No countries match your search</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Countires;
